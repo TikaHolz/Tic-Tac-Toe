1fc9b30501ae803c8136486c64e9635a579d9197
marika.holz <marika.holz@MU-C1-08.mullen.pvt.k12.co.us>
2021-09-17 11:39:20 -0600

    computer player Ai. started

--- Tic Tac Toe/src/ComputerPlayerAI.java
+++ Tic Tac Toe/src/ComputerPlayerAI.java
@@ -2,11 +2,23 @@
 public class ComputerPlayerAI
 	{
 	static int [][] hitAndWin = new int[3][3];
+	static int lineSum;
 	public static void computerPlayerAI()
 		{
 		int randomRow = (int)(Math.random()* 3);
 		int randomCol =  (int)(Math.random()* 3);
 		Board.board[randomRow][randomCol] = "O";
 		//hello how are you today!!!
+		}
+	public static void checkRow()
+		{
+		for(int rowCheck = 0; rowCheck < hitAndWin.length; rowCheck++)
+			{
+			lineSum = 0;
+			for(int colCheck = 0; colCheck < hitAndWin[0].length; colCheck++ )
+				{
+				
+				}
+			}
 		}
 	}
